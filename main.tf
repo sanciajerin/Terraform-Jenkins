3a98357c0fac2bdb16968abae306237c2644e702
Jerin <163454926+sanciajerin@users.noreply.github.com>
2024-08-01 12:56:15 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,24 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1" # You can change this to your preferred AWS region
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
+# Create an EC2 instance
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-04a81a99f5ec58529" # Example AMI, you should update this with a valid AMI for your region
+  instance_type = "t2.micro" # Specify the instance type
+  vpc_security_group_ids = ["sg-00e3c9cc3e6996a4a"]
+
   tags = {
-      Name = "Jerry-test"
+    Name = "MyEC2Instance"
+  }
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "my-jerryparakode1993-bucket" {
+  bucket = "my-jerryparakode1993-bucket"
+
+
+  tags = {
+    Name = "My-S3-Bucket"
   }
 }
